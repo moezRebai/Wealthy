8f285af561fa8d65977cf4c80a29a7d62b7a4528
Moez <Moez@LAPTOP-HA3QIQNQ>
2017-07-16 23:07:23 +0200

    Add model classes

---
+++ Source/Wealthy.Model/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Wealthy.Model
+{
+    /// <summary>
+    /// Class that represents user account.
+    /// </summary>
+    public class Account
+    {
+        public Guid AccountID { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime? DateCreated { get; set; }
+
+        public DateTime? DateUpdated { get; set; }
+        
+    }
+}
---
+++ Source/Wealthy.Model/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wealthy.Model
+{
+    /// <summary>
+    /// Class that represents the global user.
+    /// </summary>
+    public class User
+    {
+        public Guid UserID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    
+        public DateTime? DateCreated { get; set; }
+
+        public DateTime? DateUpdated { get; set; }
+
+        public virtual IList<Account> Accounts { get; set; }
+
+        public User()
+        {
+            DateCreated = DateTime.Now;
+        }
+    }
+}
